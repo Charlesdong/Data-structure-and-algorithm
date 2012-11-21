17540c419e2725469c07168ae4dde45aba759e39
charles <charles@charles.(none)>
2012-11-21 20:12:25 +0800

    second vision

--- test.py
+++ test.py
@@ -14,8 +14,21 @@ def first_test(aList):
                 MaxSum = ThisMax
     return MaxSum
 
+def second_test(aList):
+    """O(N^2)"""
+    n = len(aList)
+    MaxSum = 0
+    for i in range(0, n):
+        ThisSum = 0
+        for j in range(i, n):
+            ThisSum += aList[j]
+            if ThisMax > MaxSum: 
+                MaxSum = ThisMax
+    return MaxSum
+
 if __name__ == '__main__':
-    aList = [0, 31, -2, 4, -4]
+    aList = [-9, 10, -3]
+    #result = first_test(aList)
     result = first_test(aList)
     print "result is: ", result
 
